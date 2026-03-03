577c576f28ac9cc9fefce4610d8bca3433c7fb29
cliebentritt4 <carsonlieb52@outlook.com>
2026-03-03 14:48:50 -0600

    done

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -9,7 +9,8 @@ public class Addition
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
 		Assert.AreEqual(12, Program.Add("5", "7"));
-	}
+        Assert.AreEqual(12, Program.Add("5", "8"));
+    }
 
 	[TestMethod]
 	public void Add_Invalid_Patino()
